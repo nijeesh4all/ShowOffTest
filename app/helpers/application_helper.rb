ef211bdae3fea46636136ee0e0d9bc12d402cdb7
nijeesh <Nijeesh Joshy>
2020-02-15 00:38:46 +0530

    1. added proper route for current logged in user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_user_resource?
+    logged_in? &&
+        params[:user_id] == 'me' ||
+        (controller_name == 'users' && params[:id] == 'me') ||
+        @user&.id == @current_user&.id
+  end
+
+  def profile_path?
+    controller_name == 'users' and action_name == 'show'
+  end
+
 end

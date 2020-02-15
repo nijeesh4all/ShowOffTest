bd4e996366ca7dc7a590ab8d78aaa62d48a664c9
nijeesh <Nijeesh Joshy>
2020-02-15 11:52:04 +0530

    1. added flash message for rails flash sessions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery
-//= require vendors/jquery-bootstrap-purr.min
 //= require_tree .
 
 Turbolinks.start()
--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,18 +1,28 @@
-class Flash{
+Flash = {
+    notification: (message, type) => {
+        Toastify({
+            text: message,
+            backgroundColor: "indianred",
+            className: type,
+            position: 'center'
+        }).showToast();
+    },
 
+    error: function (message) {
+        this.notification(message, 'error')
+    },
+
+    success: function (message) {
+        this.notification(message, 'error')
+    }
 }
 
-Flash.error = (message) => {
-    $.bootstrapPurr(message, {
-        draggable: false,
-        type: 'danger'
-    });
-}
+$(document).on("turbolinks:load", function () {
+    $('.alert.danger').each((index, elem) => {
+        Flash.error(elem.innerHTML)
+    })
 
-Flash.success = (message) => {
-    $.bootstrapPurr(message, {
-        draggable: false,
-        type: 'success'
-    });
-}
-
+    $('.alert.sucess').each((index, elem) => {
+        Flash.success(elem.innerHTML)
+    })
+})

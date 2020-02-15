13185af9df9a3a27c994011ea4bc69d4ada848f7
nijeesh <Nijeesh Joshy>
2020-02-16 03:45:38 +0530

    1. configured rspec for running without ActiveRecord

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,11 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
+
+# for running rspec without ActiveRecord
+# https://stackoverflow.com/questions/46204197/testing-a-databaseless-rails-5-application-with-rspec-rails
+Object.send(:remove_const, :ActiveRecord)
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -24,20 +29,15 @@ require 'rspec/rails'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
--- spec/requests/auth_request_spec.rb
+++ spec/requests/auth_request_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Auths", type: :request do
-
+  it "redirects user back if not logged it" do
+    get user_widgets_path('me')
+    expect(response).to redirect_to root_path
+  end
 end

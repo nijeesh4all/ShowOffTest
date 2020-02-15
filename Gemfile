960c89b1fced45299e8b7270b369f79b61a3f204
nijeesh <Nijeesh Joshy>
2020-02-16 03:17:40 +0530

    1. added basic factories

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'factory_bot_rails'
   gem 'capybara'
+  gem 'faker'
 end
 
 group :development do
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    last_name { Faker::Name.last_name }
+    first_name { Faker::Name.first_name }
+    first_name { Faker::Internet.email }
+    password { 'password'}
+    password_confirmation { 'password'}
+    image_url { Faker::Avatar.image }
   end
 end
